a15b1764e91c238d2186482df5c3de73ad47324f
Александр Никитенко <PreLeks@yandex.ru>
2022-05-03 14:27:16 +0300

    ПР4 после 1-го ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,28 @@
 const openEditBtn = document.querySelector('.profile__editBtn_popup');
 const editPopup = document.querySelector('.popup');
 const closeEditBtn = document.querySelector('.popup__close');
-const saveBtn = document.querySelector('.popup__saveBtn');
-
 let formElement = document.querySelector('.popup__container');
 let nameInput = document.querySelector('.popup__input_username');
 let jobInput = document.querySelector('.popup__input_userjob');
-
 let username = document.querySelector('.profile__name');
 let userjob = document.querySelector('.profile__job');
 
-if (username.innerHTML !== "") {
-    nameInput.value = username.innerHTML;
+function openEditPopup() {
+    nameInput.value = username.textContent;
+    jobInput.value = userjob.textContent;
+    if (editPopup.classList.contains('popup_opened')) {
+        editPopup.classList.remove('popup_opened');
+    } else {
+        editPopup.classList.add('popup_opened');
+    }
 }
-
-if (userjob.innerHTML !== "") {
-    jobInput.value = userjob.innerHTML;
-}
-
-openEditBtn.addEventListener('click', function() {
-    editPopup.classList.add('popup_opened');
-});
-
-closeEditBtn.addEventListener('click', function() {
-    editPopup.classList.remove('popup_opened');
-});
-
-saveBtn.addEventListener('click', function() {
-    editPopup.classList.remove('popup_opened');
-});
-
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    nameInput.value;
-    jobInput.value;    
     username.textContent = nameInput.value;
     userjob.textContent = jobInput.value;
+    openEditPopup();
 }
 
+openEditBtn.addEventListener('click', openEditPopup);
+closeEditBtn.addEventListener('click', openEditPopup);
 formElement.addEventListener('submit', formSubmitHandler);
