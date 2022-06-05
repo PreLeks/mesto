7649e7d7cb2bbfc1995389526be03f47289ad971
Александр Никитенко <PreLeks@yandex.ru>
2022-06-05 16:04:06 +0300

    ПР6, second download

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
-const openBtnProfile = document.querySelector('.profile__editBtn_popup');
-const openBtnCard = document.querySelector('.profile__addBtn_popup');
+const profileOpeningButton = document.querySelector('.profile__editBtn_popup');
+const cardOpeningButton = document.querySelector('.profile__addBtn_popup');
 const popupProfile = document.querySelector('.popup_type_edit');
 const popupCard = document.querySelector('.popup_type_add');
 const popupPic = document.querySelector('.popup_type_pic');
-const closeBtnProfile = popupProfile.querySelector('.popup__close');
-const closeBtnCard = popupCard.querySelector('.popup__close');
-const closeBtnPic = popupPic.querySelector('.popup__close');
+const profileCloseButton = popupProfile.querySelector('.popup__close');
+const cardCloseButton = popupCard.querySelector('.popup__close');
+const picCloseButton = popupPic.querySelector('.popup__close');
 const popupPicImg = popupPic.querySelector('.popup__image');
 const popupPicTitle = popupPic.querySelector('.popup__image-title');
 const formProfile = document.querySelector('.popup__form_type_profile');
@@ -16,31 +16,30 @@ const userjob = document.querySelector('.profile__job');
 const formCard = document.querySelector('.popup__form_type_card');
 const nameInputCard = formCard.querySelector('.popup__input_titlecard');
 const linkInputCard = formCard.querySelector('.popup__input_urlcard');
-const cardsList = document.querySelector('.elements');
+const cardsContainer = document.querySelector('.elements');
 const cardTemplate = document.querySelector('.template').content.querySelector('.elements__element');
 const popupButton = formCard.querySelector('.popup__button');
 
 function closePopupEscape(evt) {
-  const openedPopup = document.querySelector('.popup_opened');
-  if (openedPopup && evt.key === 'Escape') {
-    closePopup(openedPopup);
+  if (evt.key === 'Escape') {
+    const popupOpened = document.querySelector('.popup_opened');
+    closePopup(popupOpened);
   }
 };
 function closePopupOverlay(evt) {
-  const openedPopup = document.querySelector('.popup_opened');
-  if (openedPopup && evt.target === openedPopup) {
-    closePopup(openedPopup);
+  if (evt.target === evt.currentTarget) {
+    closePopup(evt.target);
   }
 };
 function openPopup(namePopup) {
   namePopup.classList.add('popup_opened');
   document.addEventListener('keydown', closePopupEscape);
-  document.addEventListener('mousedown', closePopupOverlay);
+  namePopup.addEventListener('mousedown', closePopupOverlay);
 }
 function closePopup(namePopup) {
   namePopup.classList.remove('popup_opened');
   document.removeEventListener('keydown', closePopupEscape);
-  document.removeEventListener('click', closePopupOverlay);
+  namePopup.removeEventListener('mousedown', closePopupOverlay);
 }
 function openProfileInfo() {
   nameInputProfile.value = username.textContent;
@@ -56,10 +55,10 @@ function formProfileHandler(evt) {
 initialCards.reverse().forEach(initialCard => {
   renderCard(initialCard);
 });
-function openPopupPic(evt) {
-  popupPicImg.src = evt.target.src;
-  popupPicImg.alt = evt.target.alt;
-  popupPicTitle.textContent = evt.target.alt;
+function openPopupPic({ link, name }) {
+  popupPicImg.src = link;
+  popupPicImg.alt = name;
+  popupPicTitle.textContent = name;
   openPopup(popupPic);
 }
 function delBtnHandler(evt) {
@@ -71,31 +70,33 @@ function likeBtnHandler(evt) {
 function formCardHandler(evt) {
   evt.preventDefault();
   renderCard({ link: linkInputCard.value, name: nameInputCard.value });
-  evt.target.reset();
+  formCard.reset();
   popupButton.disabled = true;
   popupButton.classList.add('popup__button_disabled');
   closePopup(popupCard);
 }
 function createCard({ link, name }) {
   const initialCardsElement = cardTemplate.cloneNode(true);
-  initialCardsElement.querySelector('.elements__image').src = link;
-  initialCardsElement.querySelector('.elements__image').alt = name;
-  initialCardsElement.querySelector('.elements__name').textContent = name;
-  initialCardsElement.querySelector('.elements__image').addEventListener('click', openPopupPic);
-  const delBtnCard = initialCardsElement.querySelector('.elements__trash');
-  const likeBtnCard = initialCardsElement.querySelector('.elements__heart');
-  delBtnCard.addEventListener('click', delBtnHandler);
-  likeBtnCard.addEventListener('click', likeBtnHandler);
+  const cardImage = initialCardsElement.querySelector('.elements__image');
+  const cardName = initialCardsElement.querySelector('.elements__name');
+  cardImage.src = link;
+  cardImage.alt = name;
+  cardName.textContent = name;
+  cardImage.addEventListener('click', () => openPopupPic({ link, name }));
+  const cardDeletionButton = initialCardsElement.querySelector('.elements__trash');
+  const сardLikeButton = initialCardsElement.querySelector('.elements__heart');
+  cardDeletionButton.addEventListener('click', delBtnHandler);
+  сardLikeButton.addEventListener('click', likeBtnHandler);
   return initialCardsElement;
 }
 function renderCard(card) {
-  cardsList.prepend(createCard(card));
+  cardsContainer.prepend(createCard(card));
 }
 
-openBtnProfile.addEventListener('click', openProfileInfo);
-openBtnCard.addEventListener('click', () => { openPopup(popupCard) });
-closeBtnProfile.addEventListener('click', () => { closePopup(popupProfile) });
-closeBtnCard.addEventListener('click', () => { closePopup(popupCard) });
-closeBtnPic.addEventListener('click', () => { closePopup(popupPic) });
+profileOpeningButton.addEventListener('click', openProfileInfo);
+cardOpeningButton.addEventListener('click', () => { openPopup(popupCard) });
+profileCloseButton.addEventListener('click', () => { closePopup(popupProfile) });
+cardCloseButton.addEventListener('click', () => { closePopup(popupCard) });
+picCloseButton.addEventListener('click', () => { closePopup(popupPic) });
 formProfile.addEventListener('submit', formProfileHandler);
 formCard.addEventListener('submit', formCardHandler);
