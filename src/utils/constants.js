f72d09537dcfbcca2159fbe49ec8cf4571bbb510
Александр Никитенко <PreLeks@yandex.ru>
2022-07-27 13:10:20 +0300

    ПР9 first download fixtest2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,11 +9,11 @@ export const validationProps = {
 
 export const profileAbout = ('.profile__about');
 export const imageInPopup = ('.popup__image');
-export const deleteCard = ('.popup_delete-card');
+export const deleteCard = ('.popup__delete-card');
 
 export const popupAvatar = ('.popup__avatar');
 export const profileAvatar = ('.profile__image');
-export const formAvatar = document.querySelector('.popup__input_container-avatar');
+export const formAvatar = document.querySelector('.popup__form_type_avatar');
 export const editAvatarButton = document.querySelector('.profile__button-avatar');
 
 export const nameInput = document.querySelector('.popup__item_input-name');
@@ -27,12 +27,12 @@ export const editButton = document.querySelector('.profile__button-edit');
 export const addPopup = ('.popup__add');
 export const editPopup = ('.popup__edit');
 
-export const formPlace = document.querySelector('.popup__input_container-add');
-export const formAbout = document.querySelector('.popup__input_container-edit');
+export const formPlace = document.querySelector('.popup__form_type_add');
+export const formAbout = document.querySelector('.popup__form_type_edit');
 
 export const placeTemplate = ('.place-template');
 export const placeCatalogue = ('.places__catalogue');
 
-export const submitButtonPlace = document.querySelector('.popup__button_type_place');
-export const submitButtonAbout = document.querySelector('.popup__button-submit-edit');
-export const submitButtonAvatar = document.querySelector('.popup__button-submit-avatar');
+export const submitButtonPlace = document.querySelector('.popup__button-submit_type_place');
+export const submitButtonAbout = document.querySelector('.popup__button-submit_type_edit');
+export const submitButtonAvatar = document.querySelector('.popup__button-submit_type_avatar');
